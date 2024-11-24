7361c29267faf0d5affaec1e9dc7709c54398392
yasuaki-matsunaga <hachimitsu1993@hotmail.co.jp>
2024-11-24 15:45:50 +0900

    initializers/carrierwave.rbの追加

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+require 'cloudinary'
+require 'carrierwave'
+require 'carrierwave/storage/cloudinary'
+
+CarrierWave.configure do |config|
+  config.storage = :cloudinary
+  config.cloudinary_config = {
+    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
+    api_key:    ENV['CLOUDINARY_API_KEY'],
+    api_secret: ENV['CLOUDINARY_API_SECRET']
+  }
+end
