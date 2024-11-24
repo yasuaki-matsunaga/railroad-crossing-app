cb318f109bcd1f9b9cd55eadce4eda1e28d8d96a
yasuaki-matsunaga <hachimitsu1993@hotmail.co.jp>
2024-11-24 15:58:52 +0900

    initializers/carrierwave.rbの修正

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module RailroadCrossingApp
     end
 
     config.i18n.default_locale = :ja
+
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,13 @@ require 'cloudinary'
 require 'carrierwave'
 require 'carrierwave/storage/cloudinary'
 
-CarrierWave.configure do |config|
-  config.storage = :cloudinary
-  config.cloudinary_config = {
-    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-    api_key:    ENV['CLOUDINARY_API_KEY'],
-    api_secret: ENV['CLOUDINARY_API_SECRET']
-  }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :cloudinary
+    config.cloudinary_config = {
+      cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
+      api_key:    ENV['CLOUDINARY_API_KEY'],
+      api_secret: ENV['CLOUDINARY_API_SECRET']
+    }
+  end
 end
