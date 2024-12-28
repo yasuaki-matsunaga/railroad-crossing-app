91d433a0a0aebb3dc869e003089dd13ce71db628
yasuaki-matsunaga <hachimitsu1993@hotmail.co.jp>
2024-12-29 08:20:25 +0900

    crossing_infoの緯度・経度をGoogleMapリンクに

--- app/controllers/crossings_controller.rb
+++ app/controllers/crossings_controller.rb
@@ -4,7 +4,6 @@ class CrossingsController < ApplicationController
   def show
     @crossing = Crossing.includes(city: [:prefecture], linked_railways:[], posts:[]).find(params[:id])
     @posts = @crossing.posts.order(created_at: :desc)
-
   end
 
 end
