b9287c77f9ef6f3b0b4f7b84587677251c79ee86
yasuaki-matsunaga <hachimitsu1993@hotmail.co.jp>
2025-02-18 19:28:46 +0900

    路線検索のオートコンプリート

---
+++ app/controllers/railway_search_controller.rb
@@ -0,0 +1,20 @@
+class RailwaySearchController < ApplicationController
+  def search
+    @railways = Railway.where("railway_name like ?", "%#{params[:q]}%")
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_railway
+    @railway = Railway.find(params[:id])
+  end
+  # Only allow a list of trusted parameters through.
+  def railway_params
+    params.require(:railway).permit(:railway_name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'map_pages#top'
+  get 'railway_search/search', to: 'railway_search#search'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
